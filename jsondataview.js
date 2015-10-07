2b7a1d8929224649820e7b0e86c44605d20a6089
acouch <acinternets@gmail.com>
2015-10-07 15:35:39 +0000

    Adds WMS preview

--- jsondataview.js
+++ jsondataview.js
@@ -5,7 +5,7 @@
 (function ($) {
   Drupal.behaviors.Recline = {
     attach: function (context) {
-              if (typeof Drupal.settings.recline !== 'undefined') {
+              if (typeof Drupal.settings.recline !== 'undefined' && typeof Drupal.settings.recline.data !== 'undefined') {
                 var json = Drupal.settings.recline.data;
                 $('#recline-data-json').JSONView(json);
                 $('#recline-data-json').JSONView('collapse');
--- restdataview.js
+++ restdataview.js
@@ -6,7 +6,7 @@
   Drupal.behaviors.Recline = {
     attach: function (context) {
       if (typeof Drupal.settings.recline !== 'undefined' && typeof Drupal.settings.recline.url !== 'undefined') {
-        var map = L.map('rest-map').setView([33, -77], 3);
+        var map = L.map('rest-map').setView([Drupal.settings.recline.lat, Drupal.settings.recline.lon], 4);
         L.esri.basemapLayer('Gray').addTo(map);
         L.esri.dynamicMapLayer({
           url: Drupal.settings.recline.url,
@@ -14,7 +14,7 @@
           useCors: false
         }).addTo(map);
         var query = L.esri.Tasks.query({
-          url: Drupal.settings.recline.url + '0'
+          url: Drupal.settings.recline.url + '/0'
         });
         query.bounds(function(error, latLngBounds, response){
           map.fitBounds(latLngBounds);
